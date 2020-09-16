d675381f9dbbd982e96da63847a8d1f7362e119a
mark <>
2020-09-16 16:11:28 +0900

    add user fragment

---
+++ app/src/main/java/io/hwjang/app/demo/ui/jandi/UserFragment.kt
@@ -0,0 +1,19 @@
+package io.hwjang.app.demo.ui.jandi
+
+class UserFragment {
+
+
+    var list = listOf<Int>(1, 2, 3, 4, 5, 6, 7, 8, 9)
+    fun list() {
+        list.toMutableList()
+    }
+
+    fun getUser() {
+
+    }
+
+    fun showToast(message: String) {
+
+    }
+
+}
